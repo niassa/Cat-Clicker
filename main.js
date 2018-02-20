d76e87f9606c0fc77f196a585b885c3e3adb78c8
niassa <niassa@gmail.com>
2018-02-20 13:12:31 -0700

    Initial Commit

---
+++ main.js
@@ -0,0 +1,44 @@
+// this is my javascript
+
+var $catID = "";
+var $countID = "";
+
+
+var cat = {
+	name: ["Kitty One", "Kitty Two", "Kitty Three"],
+	imgURL: ["cat.jpg", "cat2.jpg", "cat.jpg"],
+	imgAlt: ["Cute cat #1", "Cute cat #2", "Cute Cat #3"],
+	id: ["cat1", "cat2", "cat3"],
+	count: [0,0,0],
+	display: function() {
+		for(var i = 0; i < cat.name.length; i++) {
+			$catID = "#" + cat.id[i];
+			$countID = "#count" + i;
+			var header = '<h2>' + cat.name[i] + '</h2>';
+			var image = '<img src="' + cat.imgURL[i] + '" alt="' + cat.imgAlt[i] + '" id="' + cat.id[i] + '">';
+			var pText = '<p>You have clicked the cat <span id="count' + i + '">0</span> times!</p>';
+			$('.kitties').append(header + image + pText);
+		}
+	}
+};
+
+cat.display();
+
+// Function to make cat picture clickable and trace clicks
+$('body').click(function(e) {
+
+    var target = $(e.target);
+
+    for (var i = 0; i < cat.name.length; i++) {
+    	var ifTarget = "#" + cat.id[i];
+    	// console.log(ifTarget);
+    	if (target.is(ifTarget)) {
+    		var newCount = cat.count[i];
+    		newCount = newCount+1;
+    		cat.count[i] = newCount;
+    		newCount.toString();
+    		$countID = "#count" + i;
+    		$($countID).text(newCount);
+		}
+	}
+});
