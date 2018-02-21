d7ff3fc789365739ef9d996dfa3955eea7b2152f
niassa <niassa@gmail.com>
2018-02-20 20:33:35 -0700

    Updated for 2nd Requirements Change

--- js/main.js
+++ js/main.js
@@ -2,23 +2,29 @@
 
 var $catID = "";
 var $countID = "";
-
+var startSel = '<select id="catSelect">';
+var endSel = '</select>';
+var optTagOpen = '<option value="';
+var optTagBlank = '<option value="blank">Select a picture from the list</option>';
+var optTagClose = '</option>';
+var blankTxt = '<strong>Select a cat image from the drop-down to the right.</strong>';
 
 var cat = {
-	name: ["Kitty One", "Kitty Two", "Kitty Three"],
-	imgURL: ["imgs/cat.jpg", "imgs/cat2.jpg", "imgs/cat.jpg"],
-	imgAlt: ["Cute cat #1", "Cute cat #2", "Cute Cat #3"],
-	id: ["cat1", "cat2", "cat3"],
-	count: [0,0,0],
-	display: function() {
+	name: ["Severus Asleep in Front of Fireplace", "Severus Wants Steak", "Severus Won't Let Me Work", "Severus is a Pretty Kitty", "Severus Has Huge Paws!"],
+	imgURL: ["imgs/fireplaceCat.jpg", "imgs/iWantSteak.jpg", "imgs/lyingOnComp.jpg", "imgs/prettyKitty.jpg", "imgs/sittingOnArm.jpg"],
+	imgAlt: ["Severus curled up in front of the fireplace", "Severus staring at a steak on a plate", "Severus Lying on a Computer", "Severus reaching out a paw", "Severus with one paw extended down"],
+	id: ["cat1", "cat2", "cat3", "cat4", "cat5"],
+	count: [0,0,0,0,0],
+
+	display: function() { //This is the function that actually loads the display initially
+		$('.content').append(blankTxt);
+		$('.right-sidebar').append(startSel + optTagBlank);
 		for(var i = 0; i < cat.name.length; i++) {
-			$catID = "#" + cat.id[i];
-			$countID = "#count" + i;
-			var header = '<h2>' + cat.name[i] + '</h2>';
-			var image = '<img src="' + cat.imgURL[i] + '" alt="' + cat.imgAlt[i] + '" id="' + cat.id[i] + '">';
-			var pText = '<p>You have clicked the cat <span id="count' + i + '">0</span> times!</p>';
-			$('.kitties').append(header + image + pText);
+			var optionVal = cat.id[i];
+			var optionName = cat.name[i];
+			$('#catSelect').append(optTagOpen + optionVal + '">' + optionName + optTagClose)
 		}
+		$('.right-sidebar').append(endSel);
 	}
 };
 
@@ -27,10 +33,10 @@ cat.display();
 // Function to make cat picture clickable and trace clicks
 $('body').click(function(e) {
 
-    var target = $(e.target);
+	var target = $(e.target);
 
-    for (var i = 0; i < cat.name.length; i++) {
-    	var ifTarget = "#" + cat.id[i];
+	for (var i = 0; i < cat.name.length; i++) {
+		var ifTarget = "#" + cat.id[i];
     	// console.log(ifTarget);
     	if (target.is(ifTarget)) {
     		var newCount = cat.count[i];
@@ -39,6 +45,29 @@ $('body').click(function(e) {
     		newCount.toString();
     		$countID = "#count" + i;
     		$($countID).text(newCount);
-		}
+    	}
+    }
+});
+
+// This is the function that determines which cat is selected from the drop-down list, and then changes the cat picture and info to the selected cat.
+var $catSelect = $("#catSelect");
+$catSelect.change(function() {
+	var $contentChild = $('.content').children();
+
+	for (var i = 0; i < $contentChild.length; i++) {
+		$contentChild[i].remove();
+	}
+	if ($catSelect.val() === "blank") {
+		$('.content').append(blankTxt);
+	} else {
+		var catNum = $catSelect.val().split("cat")[1];
+		parseInt(catNum);
+		catNum--;
+		$catID = "#" + cat.id[catNum];
+		$countID = "#count" + catNum;
+		var header = '<h2>' + cat.name[catNum] + '</h2>';
+		var image = '<img src="' + cat.imgURL[catNum] + '" alt="' + cat.imgAlt[catNum] + '" id="' + cat.id[catNum] + '">';
+		var pText = '<p>You have clicked the cat <span id="count' + catNum + '">'+ cat.count[catNum] + '</span> times!</p>';
+		$('.content').append(header + image + pText);
 	}
 });
